904edb83154f84233f18703b0a58343af0a2a8eb
Dzmitry Yanusik <yanusikdmitry@gmail.com>
2023-08-02 00:28:18 +0200

    Improved wiki clients

--- htmlwikiclient/src/main/java/com/lastminute/recruitment/client/HtmlWikiClient.java
+++ htmlwikiclient/src/main/java/com/lastminute/recruitment/client/HtmlWikiClient.java
@@ -1,12 +1,26 @@
 package com.lastminute.recruitment.client;
 
-import java.util.Objects;
+import com.lastminute.recruitment.domain.error.WikiPageNotFound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.Optional;
 
 public class HtmlWikiClient {
 
-    public String readHtml(String link) {
+    private static final Logger LOG = LoggerFactory.getLogger(HtmlWikiClient.class);
+
+    public String readFileName(String link) {
         String name = link.replace("\"", "")
                 .replace("http://wikiscrapper.test/", "/wikiscrapper/") + ".html";
-        return Objects.requireNonNull(getClass().getResource(name)).getFile();
+        Optional<String> fileName = Optional.ofNullable(getClass().getResource(name)).map(URL::getFile);
+
+        if (fileName.isEmpty() || fileName.get().isEmpty()) {
+            LOG.info("Cannot find file by provided link : %s".formatted(link));
+            throw new WikiPageNotFound();
+        } else {
+            return fileName.get();
+        }
     }
 }
