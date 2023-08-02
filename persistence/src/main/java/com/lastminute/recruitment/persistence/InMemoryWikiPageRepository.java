84e7803871fe1c6e33870b20ecdf72836989df83
Dzmitry Yanusik <yanusikdmitry@gmail.com>
2023-08-02 13:59:01 +0200

    Fix concurrency issue in wiki repository

--- persistence/src/main/java/com/lastminute/recruitment/persistence/InMemoryWikiPageRepository.java
+++ persistence/src/main/java/com/lastminute/recruitment/persistence/InMemoryWikiPageRepository.java
@@ -5,16 +5,16 @@ import com.lastminute.recruitment.domain.WikiPageRepository;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryWikiPageRepository implements WikiPageRepository {
 
-    private long idCounter = 0;
+    private final AtomicLong idCounter = new AtomicLong();
     private final Map<Long, WikiPage> db = new HashMap<>();
-
 
     @Override
     public void save(WikiPage wikiPage) {
-        db.put(idCounter++, wikiPage);
+        db.put(idCounter.incrementAndGet(), wikiPage);
         System.out.println("WikiPages in DB:");
         db.keySet().forEach(wikiPageKey ->
                 System.out.println(wikiPageKey + "->" + db.get(wikiPageKey))
